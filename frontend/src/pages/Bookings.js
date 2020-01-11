dc5975246bf89e547730a3a4086ee883ba337758
kenthow@gmail.com <kenthow@gmail.com>
2020-01-11 16:52:29 -0500

    added front-end basics

---
+++ frontend/src/pages/Bookings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class BookingsPage extends Component {
+  render() {
+    return(
+      <h1>the Bookings page</h1>
+    );
+  }
+}
+
+export default BookingsPage;
+
---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class EventsPage extends Component {
+  render() {
+    return(
+      <h1>the Events page</h1>
+    );
+  }
+}
+
+export default EventsPage;
+
